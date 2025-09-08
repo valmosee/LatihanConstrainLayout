a9043bc19c1c0151af960cab0bbbf7ab2e9a7bed
Joice <c14230225@john.petra.ac.id>
2025-09-08 10:32:24 +0700

    update

--- app/src/main/java/com/example/latihanconstrainlayout/MainActivity.kt
+++ app/src/main/java/com/example/latihanconstrainlayout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.latihanconstrainlayout
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
